d6f288d1e64d9e119d2f2be3ec71009b6f721c23
James Anderson <jan@etepd.com>
2025-02-27 19:04:38 -0300

    Adição da questão 1 da apostila 5.

---
+++ Problemas Resolvidos/Apostila 5/question_01.py
@@ -0,0 +1,13 @@
+class Aluno:
+ def __init__(self, nome, nota):
+    self.nome = nome
+    self.nota = nota
+ 
+ def apresentar(self):
+     if(self.nota >= 7):print(f'Olá, meu nome é {self.nome} e a minha nota é {self.nota}. Situação do aluno(a): APROVADO!')
+     else:print(f'Olá, meu nome é {self.nome} e a minha nota é {self.nota} anos. Situação do aluno(a): REPROVADO!')
+ 
+nome_do_aluno = input('Digite o nome do aluno(a): ')
+nota_do_aluno = float(input('Digite a nota do aluno(a): '))
+aluno1 = Aluno(nome_do_aluno, nota_do_aluno)
+aluno1.apresentar()
