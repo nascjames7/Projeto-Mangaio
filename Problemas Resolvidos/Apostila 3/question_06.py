a3a2236159377f8025f365de663730fd5da48a03
James Anderson <jan@etepd.com>
2025-02-27 17:46:04 -0300

    Adição da questão 6 da apostila 3.

---
+++ Problemas Resolvidos/Apostila 3/question_06.py
@@ -0,0 +1,7 @@
+pet = {'Tipo': 'Canis familiares', 'Nome': 'Rex', 'Idade(anos)': 4, 'Raça': 'Labrador', 'Cor': 'branco'}
+print('Os dados do pet: ')
+print('Tipo: ', pet['Tipo'])
+print('Nome: ', (pet['Nome']))
+print('Idade(anos): ', (pet['Idade(anos)']))
+print('Raça: ', (pet['Raça']))
+print('Cor: ', (pet['Cor']))
