86bf4758513502a687d04c958557433873b62c90
James Anderson <jan@etepd.com>
2025-03-03 14:48:01 -0300

    Finalização da questão 8 da apostila 2.

--- Problemas Resolvidos/Apostila 2/question_08.py
+++ Problemas Resolvidos/Apostila 2/question_08.py
@@ -1,9 +1,11 @@
 import random
+
+if(True):pass
       
 while True:
     numero = random.randint(1,1000) 
     print(numero)    
     if numero > 900: break
     if numero < 500: continue
-    #if numero >= 500   
+       
     
